89e4d57f9362a455013c2b7b9e6791f69c663785
Brian Blais <bblais@gmail.com>
2024-01-30 12:55:01 -0500

    first

---
+++ Sprint #1 - Algorithms and Simulation/2024-01-30 - First Notebook.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Header
+# 
+# ## Subheading
+# 
+# some text.
+# 
+# ### sub sub heading
+# 
+# ## another subheading
+# 
+# I can do **bold** and *italic*.  Even equations (use LaTeX format):
+# 
+# $$
+# E=mc^2 \times \sqrt{\frac{1}{1-v^2/c^2}}
+# $$
+# 
+
+# In[1]:
+
+
+a=5
+
+
+# In[2]:
+
+
+a
+
+
+# In[3]:
+
+
+b=10
+a+b
+
+
+# In[4]:
+
+
+a*b
+
+
+# Exponents use ** not ^
+
+# In[5]:
+
+
+3**4
+
+
+# In[6]:
+
+
+3^4  # gives a weird answer and is not an exponent
+
+
+# In[7]:
+
+
+a
+
+
+# In[8]:
+
+
+a=a+1
+
+
+# In[9]:
+
+
+a
+
+
+# In[11]:
+
+
+bob=56
+sally=103
+bob*sally
+
+
+# In[13]:
+
+
+print("here")
+for i in range(10):
+    print(i)
+    print("hello")
+
+    print("another")
+
+print("there")
+
+
+# In[14]:
+
+
+print("here")
+for bob in range(10):
+    print(bob,"hello")
+
+print("there")
+
+
+# In[16]:
+
+
+S=0
+for value in range(10):
+    print("before",S)
+    S=S+value
+    print("after",S)
+
+print(S)
+
+
+# In[17]:
+
+
+0+1+2+3+4+5+6+7+8+9
+
+
+# In[ ]:
+
+
+N=15
+
+# rest of this I want at the end to print out the factorial of N
+
+
