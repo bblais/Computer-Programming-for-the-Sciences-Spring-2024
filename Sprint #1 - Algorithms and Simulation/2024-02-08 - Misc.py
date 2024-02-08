a53b2d58422e568de5d069e5785bd4c2324ed90d
Brian Blais <bblais@gmail.com>
2024-02-08 13:21:18 -0500

    max by eye

---
+++ Sprint #1 - Algorithms and Simulation/2024-02-08 - Misc.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from pylab import *
+from sci378 import *
+
+
+# In[2]:
+
+
+# y=a*sin^2(θ)
+
+
+# In[7]:
+
+
+S=Storage()
+
+θ=0
+dθ=0.1
+a=1
+
+while θ<180:
+
+    y=a*sin(radians(θ))**2
+    S+=θ,y
+
+    θ=θ+dθ
+
+θ,y=S.arrays()
+plot(θ,y)
+
+
+# In[9]:
+
+
+for a in [1,2,3,4]:
+    S=Storage()
+    
+    θ=0
+    dθ=0.1
+    #a=1
+    
+    while θ<180:
+    
+        y=a*sin(radians(θ))**2
+        S+=θ,y
+    
+        θ=θ+dθ
+    
+    θ,y=S.arrays()
+    plot(θ,y)X
+
+
+# In[11]:
+
+
+for a in [1,2,3,4]:
+    θ=linspace(0,180,400)
+    y=a*sin(radians(θ))**2
+    plot(θ,y)
+
+
+# In[ ]:
+
+
+
+
