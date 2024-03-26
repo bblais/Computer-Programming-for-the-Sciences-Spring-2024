689a618af30fa165f88dfc459e05273af70ef427
Brian Blais <bblais@gmail.com>
2024-03-26 13:39:49 -0400

    mcmc

--- Sprint #3 - Probability and Uncertainty/2024-03-26 - Best Estimate.py
+++ Sprint #3 - Probability and Uncertainty/2024-03-26 - Best Estimate.py
@@ -9,6 +9,8 @@ from pylab import *
 from sci378.stats import *
 
 
+# ## Best Estimate, $\mu$, knowing $\sigma$
+
 # In[ ]:
 
 
@@ -78,6 +80,124 @@ model.plot_distributions()
 
 
 model.P("μ>101.5")
+
+
+# ## Best Estimate, unknown $\mu$ and $\sigma$
+
+# In[ ]:
+
+
+def logprior(μ,σ):
+    value=0
+    
+    value+=logNormal(μ,0,100)
+    value+=logJeffreys(σ)
+    
+    return value
+
+def loglikelihood(data,μ,σ):
+    x=data
+    value=0
+        
+    value+=logNormal(x-μ,0,σ)
+    return value
+    
+
+
+# In[ ]:
+
+
+x=drug
+model=MCMCModel(x,loglikelihood,logprior,
+               μ=Normal(10,10),    # initial guess for μ
+               σ=Uniform(0,10))   # initial guess for σ (make sure it's positive)
+
+
+# In[ ]:
+
+
+model.run_mcmc(400,repeat=3,verbose=True)
+model.plot_chains()
+
+
+# In[ ]:
+
+
+model.plot_distributions()
+
+
+# In[ ]:
+
+
+model.P("σ<5")
+
+
+# In[ ]:
+
+
+model.triangle_plot()
+
+
+# ## Two samples
+
+# In[ ]:
+
+
+def logprior(μ1,σ1,μ2,σ2):
+    value=0
+    
+    value+=logNormal(μ1,0,100)
+    value+=logJeffreys(σ1)
+    value+=logNormal(μ2,0,100)
+    value+=logJeffreys(σ2)
+    
+    return value
+
+def loglikelihood(data,μ1,σ1,μ2,σ2):
+    x,y=data
+    value=0
+        
+    value+=logNormal(x-μ1,0,σ1)
+    value+=logNormal(y-μ2,0,σ2)
+    
+    return value
+    
+
+
+# In[ ]:
+
+
+model=MCMCModel((drug,placebo),loglikelihood,logprior,
+               μ1=Normal(10,10),    # initial guess for μ1
+               σ1=Uniform(0,10),    # initial guess for σ1 (make sure it's positive)
+               μ2=Normal(10,10),    # initial guess for μ2
+               σ2=Uniform(0,10),    # initial guess for σ2 (make sure it's positive)
+               )
+
+
+# In[ ]:
+
+
+model.run_mcmc(400,repeat=3,verbose=True)
+model.plot_chains()
+
+
+# In[ ]:
+
+
+model.plot_distributions()
+
+
+# In[ ]:
+
+
+model.plot_distributions('μ1-μ2')
+
+
+# In[ ]:
+
+
+model.P('μ1-μ2<0')
 
 
 # In[ ]:
