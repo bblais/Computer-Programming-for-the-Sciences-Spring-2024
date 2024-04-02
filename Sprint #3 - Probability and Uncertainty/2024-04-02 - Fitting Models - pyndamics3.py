e4b173fc74bbc1fe337ac8bb1423e67ac7a6a7f4
Brian Blais <bblais@gmail.com>
2024-04-02 12:16:53 -0400

    class 04-02

---
+++ Sprint #3 - Probability and Uncertainty/2024-04-02 - Fitting Models - pyndamics3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from pylab import *
+from sci378 import *
+from sci378.stats import *
+
+
+# In[ ]:
+
+
+data=pd.read_csv('data/dog_mass.csv')
+t_data=data['days'].values[:10]  # just the start
+y_data=data['mass'].values[:10]
+data
+
+
+# In[ ]:
+
+
+plot(t_data,y_data,'o')
+
+
+# In[ ]:
+
+
+
+
