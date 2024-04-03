656ce53c042b3099ceb78dbb5b0adf3932f9fcce
Brian Blais <bblais@gmail.com>
2024-04-03 11:42:57 -0400

    fixed bugs

--- Sprint #3 - Probability and Uncertainty/2024-04-03 - Fitting Models - pyndamics mcmc.py
+++ Sprint #3 - Probability and Uncertainty/2024-04-03 - Fitting Models - pyndamics mcmc.py
@@ -38,7 +38,7 @@ from pyndamics3.fit import fit,Parameter
 
 
 sim=Simulation()
-sim.add("y'=a*y*(1-y/K)",initial_value=1,plot=True)
+sim.add("y'=a*y*(1-y/K)",initial_value=10,plot=True)
 sim.params(a=1,K=30)
 sim.add_data(t=t_data,y=y_data,plot=True)
 sim.run(90)
@@ -78,6 +78,43 @@ def logprior(a,K,initial_y,σ):
 def loglikelihood(sim,a,K,initial_y,σ):
     # pyndamics already has the data in the Simulation object
     err=sim.err(a=a,K=K,initial_y=initial_y)
+    return logNormal(err,0,σ)
+
+
+# In[ ]:
+
+
+model=MCMCModel(sim,loglikelihood,logprior)
+
+
+# In[ ]:
+
+
+model.run_mcmc(300,repeat=3,verbose=True)
+model.plot_chains()
+
+
+# In[ ]:
+
+
+model.plot_distributions()
+
+
+# In[ ]:
+
+
+def logprior(a,K,σ):
+    value=0. 
+    
+    value+=logUniform(a,0,1)
+    value+=logNormal(K,100,100,all_positive=True)
+    value+=logJeffreys(σ)
+    
+    return value
+
+def loglikelihood(sim,a,K,σ):
+    # pyndamics already has the data in the Simulation object
+    err=sim.err(a=a,K=K)
     return logNormal(err,0,σ)
 
 
