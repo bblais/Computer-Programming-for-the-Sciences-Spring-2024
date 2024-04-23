4d0bf910234984f1c0db02f6bfc175742cdc5e5e
Brian Blais <bblais@gmail.com>
2024-04-23 13:34:49 -0400

    class

--- Sprint #4 - Scientific Questions/2024-04-23 - Class.py
+++ Sprint #4 - Scientific Questions/2024-04-23 - Class.py
@@ -16,6 +16,8 @@ y=array([.5,3,2,4.1,.7])  # this is logged data
 y=10**y
 
 t=array([0,1,2,3,4])
+t_data=t
+y_data=y
 
 
 # In[ ]:
@@ -31,6 +33,24 @@ plot(t,y,'-o')
 figure(figsize=(6,3))
 plot(t,y,'-o')
 yscale('log')
+
+
+# In[ ]:
+
+
+from pyndamics3 import Simulation
+
+
+# In[ ]:
+
+
+sim=Simulation()
+sim.add("T'=-β*T*V",initial_value=1,plot=True)
+sim.add("I'=β*T*V-δ*I",initial_value=1,plot=True)
+sim.add("V'=p*I-c*V",initial_value=1,plot=True)
+sim.add_data(t=t_data,V=log(y_data),plot=True)
+sim.params(β=0.2,δ=0.1,p=0.5,c=0.1)
+sim.run(7)
 
 
 # In[ ]:
