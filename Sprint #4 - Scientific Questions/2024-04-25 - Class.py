a61c7de0b4ce2018343c38859027e655b15a8478
Brian Blais <bblais@gmail.com>
2024-04-25 14:22:24 -0400

    clss

--- Sprint #4 - Scientific Questions/2024-04-25 - Class.py
+++ Sprint #4 - Scientific Questions/2024-04-25 - Class.py
@@ -33,7 +33,7 @@ y_data=y
 
 sim=Simulation()
 sim.add("T'=-β*T*V",initial_value=1e9,plot=True) # uninfected
-sim.add("I'=β*T*V-δ*I",initial_value=1,plot=True) # infected
+sim.add("I'=β*T*V-δ*I",initial_value=.01,plot=True) # infected
 sim.add("V'=p*I-c*V",initial_value=0,plot=True)  # viral load
 sim.add_data(t=t_data,V=y_data,plot=True)
 sim.params(β=3.4e-3,δ=3.4,p=7.9e-3,c=3.3) #(β=0.2,δ=0.1,p=0.5,c=0.1)
